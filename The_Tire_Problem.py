3220225520bc7d24610759a001b262fd6e3cfa6f
Shivam Goel <Shivam0922@gmail.com>
2023-10-25 16:31:59 -0400

    Given values until 40,000

--- The_Tire_Problem.py
+++ The_Tire_Problem.py
@@ -1,26 +1,54 @@
-import numpy as np
-import scipy.stats as stats
+import math
 
-def probability(number, mean, standard_deviation):
-  """Calculates the probability of a specific number occurring from 1-40000 on a bell curve, given that the mean = 46,000 and standard deviation is 8600.
+def calculate_probability(mean: float, std_dev: float, number: int) -> float:
+    """
+    Calculates the probability of a number on a bell curve given the mean and standard deviation.
 
-  Args:
-    number: The number to calculate the probability for.
-    mean: The mean of the bell curve.
-    standard_deviation: The standard deviation of the bell curve.
+    Parameters:
+    - mean: float
+        The mean value of the bell curve.
+    - std_dev: float
+        The standard deviation of the bell curve.
+    - number: int
+        The number for which the probability is to be calculated.
 
-  Returns:
-    The probability of the number occurring.
-  """
+    Returns:
+    - float:
+        The probability of the number on the bell curve.
 
-  z_score = (number - mean) / standard_deviation
-  return stats.norm.cdf(z_score)
+    Raises:
+    - ValueError:
+        Raises an error if the standard deviation is zero or negative.
+    """
 
-Num1 = 1
+    # Checking if the standard deviation is valid
+    if std_dev <= 0:
+        raise ValueError("Standard deviation should be positive.")
 
-while Num1 < 40001:
-  probability = probability(Num1, 46000, 8600)
-  
-  # Print the probability.
-  print(probability)
-  Num1 += 1
+    # Calculating the z-score
+    z_score = (number - mean) / std_dev
+
+    # Calculating the probability using the cumulative distribution function (CDF)
+    probability = (1 + math.erf(z_score / math.sqrt(2))) / 2
+
+    return probability
+
+# Example usage:
+mean = 46000
+std_dev = 8600
+
+# Calculating the probability for a specific number
+number = 40000
+probability = calculate_probability(mean, std_dev, number)
+print(f"The probability of the number {number} on the bell curve is {probability}.")
+
+# Calculating the probabilities for numbers 1 to 40000
+probabilities = []
+for number in range(1, 40001):
+    probability = calculate_probability(mean, std_dev, number)
+    probabilities.append(probability)
+
+# Printing the probabilities for numbers 1 to 40000
+print("Probabilities:")
+for number, probability in enumerate(probabilities, start=1):
+    print(f"Number: {number}, Probability: {probability}")
