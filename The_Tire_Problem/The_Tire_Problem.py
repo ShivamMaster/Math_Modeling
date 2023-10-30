9fe00967c64bd36d62a1bdd7e5e83d03ca958f7f
Shivam Goel <Shivam0922@gmail.com>
2023-10-30 15:16:29 -0400

    find the difference between the probabilities

--- The_Tire_Problem/The_Tire_Problem.py
+++ The_Tire_Problem/The_Tire_Problem.py
@@ -6,8 +6,6 @@ from scipy.stats import norm
 # Define varibles for user to input thier needed numbers
 mean = 46000   # Mean life of a tire
 std_dev = 8600  # Standard Deviation of life of a tire 
-lower_bound = 0  # Minimum Miles Driven under the guarantee from the company
-upper_bound = 40000  # Maximum Miles Driven under the guarantee from the company
 details = True    # Whether or not to print the details/calculations of the program
 
 
@@ -33,7 +31,13 @@ for number in range(1, 40001):
     probability = calculate_probability(mean, std_dev, number)
     probabilities.append(probability)
 
+# Find the difference between each of the calculated probabilities
+probability_differences = []
+for i in range(len(probabilities) - 1):
+    probability_difference = probabilities[i + 1] - probabilities[i]
+    probability_differences.append(probability_difference)
 
+    
     
     
     
@@ -56,7 +60,7 @@ for number in range(1, 40001):
 
 # Calcuating the cost for the manufactoring company (our company)
 def cost(number):
-    return (probability * 100 * calculate_result(number))
+    return (probability * calculate_result(number))
 
 
 
@@ -67,59 +71,17 @@ def cost(number):
 if details == True:
     print("Probabilities:")
     for number, probability in enumerate(probabilities, start=1):
-        print(f"Number: {number}, Probability: {probability * 100}, Refund {calculate_result(number)}, Cost {cost(number)}")
+        print(f"Number: {number}, Probability: {probability_difference}, Refund {calculate_result(number)}, Cost {cost(number)}")
     
 
 
 
-
-
-
-# Average cost for each tire (not including the probability of when a tire will fail)
+# Sum all of the costs
 total_cost = 0
-for number, probability in enumerate(probabilities, start=1):
-  cost_amount = cost(number)
-  total_cost += cost_amount
+for number in range(1, 40001):
+    total_cost += cost(number)
 
-average_cost_raw = total_cost/40000
-if details == True:
-    print("The raw average cost of a tire, when not including when the tires are likely to fail is {}".format(average_cost_raw))
+# Print the total cost
+print("The avergae cost for the company is ${} per tire".format(total_cost/40000))
 
-
-
-
-
-
-
-# Finding Area Under Normal Curve to predict the tire fail rate
-
-
-def find_area_under_normal_curve(mean, std_dev, lower_bound, upper_bound):
-  
-
-  # Calculate the cumulative distribution function (CDF) at the two bounds.
-  cdf_lower = norm.cdf(lower_bound, loc=mean, scale=std_dev)
-  cdf_upper = norm.cdf(upper_bound, loc=mean, scale=std_dev)
-
-  # The area under the curve between the two bounds is the difference between the two CDF values.
-  area = cdf_upper - cdf_lower
-
-  return area
-
-area = find_area_under_normal_curve(mean, std_dev, lower_bound, upper_bound)
-
-if details == True:
-    print("The area under the normal distribution curve between {} and {} is {}".format(lower_bound, upper_bound, area))
-
-
-
-
-
-
-# General Cost for the Company:
-
-general_cost = average_cost_raw * area
-
-
-print("The program will generally cost the company ${} for every tire sold".format(general_cost))
 
