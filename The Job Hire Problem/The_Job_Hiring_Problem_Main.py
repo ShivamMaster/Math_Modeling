6f396a10862b1f0d7231c56ef04fb15861bd70f4
Shivam Goel <Shivam0922@gmail.com>
2024-03-04 17:01:24 -0500

    Added User Input for which method they want

--- The Job Hire Problem/The_Job_Hiring_Problem_Main.py
+++ The Job Hire Problem/The_Job_Hiring_Problem_Main.py
@@ -2,8 +2,35 @@ import math
 from random import randrange
 import numpy as np
 from scipy.stats import norm
+import sys
 
 
+
+#Asks the user what method random sampling that they would like (select the first person that hit above a certain threshold or use the standard deviation and predict which on is the best based on probabilities)
+# In this section of the Code, Sampling Method #1 is trying to pick the best applicant based on a minimum threshold that the user wants. So it would be the first applicant that has a score greater or equal to that number will be selected
+
+
+
+# name = input("What is your name? ")
+# age = int(input("How old are you? "))  # Convert number to integer
+
+# print("Hello, " + name + "! You are " + str(age) + " years old.")
+
+SamplingMethod1 = input("Would you like do use the method of selecting the first person that hits above a certain threshold? Type 'Y' for Yes and 'N' for No:   ")
+
+if SamplingMethod1 == "Y" or "y":
+    SamplingMethod1Status = True
+    SamplingMethod1Threshold = input("What would you like the minimum threshold to be for your applicants:  ")
+else:
+    SamplingMethod1Status = False
+    SamplingMethod2 = input("Would you like do use the method of basically auto-rejecting applicant #1 and then using stats and normal distribution curves to figure out the best applicant? Type 'Y' for Yes and 'N' for No:   ")
+    if SamplingMethod2 == "Y" or "y":
+        SamplingMethod2Status = True
+        
+    else:
+        SamplingMethod2Status = False
+        print("Error: You have not selected either methods. Please try again or contact Shivam regarding adding another method if you are not happy with the current 2 methods")
+     
 
 # Random Assigns the scores of the Applicants:
 Applicant_1 = randrange(1,100)
@@ -52,10 +79,16 @@ print("Applicant 18's score is:", Applicant_18)
 print("Applicant 19's score is:", Applicant_19)
 print("Applicant 20's score is:", Applicant_20)
 
-# Applicant #1's decision
-if Applicant_1 == 100:
-    print("Applicant 1 has been selected do not care about no else")
-else:
-    print ("Applicant 1 has not been selected and has been reject :(")
+
+# Method #1
+if SamplingMethod1Status == True:
+    # Applicant #1's decision
+    if Applicant_1 == 100:
+        print("Applicant 1 has been selected do not care about no else")
+    else:
+        print ("Applicant 1 has not been selected and has been reject :(")
+
+
+
 
 
