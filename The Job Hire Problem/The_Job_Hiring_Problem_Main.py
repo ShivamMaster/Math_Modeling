8a04ca4bcaf73d6aa2f0febe17983613561c5873
Shivam Goel <Shivam0922@gmail.com>
2024-03-04 16:30:51 -0500

    Generated random scores for applicants

---
+++ The Job Hire Problem/The_Job_Hiring_Problem_Main.py
@@ -0,0 +1,57 @@
+import math
+from random import randrange
+import numpy as np
+from scipy.stats import norm
+
+
+
+# Random Assigns the scores of the Applicants:
+Applicant_1 = randrange(1,100)
+Applicant_2 = randrange(1,100)
+Applicant_3 = randrange(1,100)
+Applicant_4 = randrange(1,100)
+Applicant_5 = randrange(1,100)
+Applicant_6 = randrange(1,100)
+Applicant_7 = randrange(1,100)
+Applicant_8 = randrange(1,100)
+Applicant_9 = randrange(1,100)
+Applicant_10 = randrange(1,100)
+Applicant_11 = randrange(1,100)
+Applicant_12 = randrange(1,100)
+Applicant_13 = randrange(1,100)
+Applicant_14 = randrange(1,100)
+Applicant_15 = randrange(1,100)
+Applicant_16 = randrange(1,100)
+Applicant_17 = randrange(1,100)
+Applicant_18 = randrange(1,100)
+Applicant_19 = randrange(1,100)
+Applicant_20 = randrange(1,100)
+
+
+
+
+# Printed the Applicant's scores to make it easier to track
+print("Applicant 1's score is:", Applicant_1)
+print("Applicant 2's score is:", Applicant_2)
+print("Applicant 3's score is:", Applicant_3)  
+print("Applicant 4's score is:", Applicant_4)
+print("Applicant 5's score is:", Applicant_5)
+print("Applicant 6's score is:", Applicant_6)
+print("Applicant 7's score is:", Applicant_7)
+print("Applicant 8's score is:", Applicant_8)
+print("Applicant 9's score is:", Applicant_9)  
+print("Applicant 10's score is:", Applicant_10)
+print("Applicant 11's score is:", Applicant_11)
+print("Applicant 12's score is:", Applicant_12)
+print("Applicant 13's score is:", Applicant_13)
+print("Applicant 14's score is:", Applicant_14)
+print("Applicant 15's score is:", Applicant_15)  
+print("Applicant 16's score is:", Applicant_16)
+print("Applicant 17's score is:", Applicant_17)
+print("Applicant 18's score is:", Applicant_18)
+print("Applicant 19's score is:", Applicant_19)
+print("Applicant 20's score is:", Applicant_20)
+
+
+
+
